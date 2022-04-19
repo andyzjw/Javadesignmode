cf2465e227e055ab0c65d469283ef6a8902908ba
andy <43564833+andyzjw@users.noreply.github.com>
2022-04-19 10:12:39 +0800

    Update FactoryMode.java
    tt

--- src/com/creation/FactoryMode.java
+++ src/com/creation/FactoryMode.java
@@ -16,7 +16,7 @@ public class FactoryMode {
 class SendFactoryNormal{
 	public Sender produce(String type) {
 		if("mail".equalsIgnoreCase(type)) {
-			System.out.println("i am coming");
+			System.out.println("i am coming 123123");
 			return new MailSender();
 		}else if("sms".equalsIgnoreCase(type)) {
 			int i =1;
