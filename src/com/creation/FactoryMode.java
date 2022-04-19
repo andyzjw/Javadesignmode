749073f72e4e81b5e3cb2ce28e58a1042e202ad1
zjw <511145881@qq.com>
2022-04-19 10:40:03 +0800

    ss

--- src/com/creation/FactoryMode.java
+++ src/com/creation/FactoryMode.java
@@ -16,11 +16,8 @@ public class FactoryMode {
 class SendFactoryNormal{
 	public Sender produce(String type) {
 		if("mail".equalsIgnoreCase(type)) {
-			System.out.println("i am coming");
 			return new MailSender();
 		}else if("sms".equalsIgnoreCase(type)) {
-			int i =1;
-			int i =2;
 			return new SmsSender();
 		}
 		System.out.println("未找到输入的类型");
